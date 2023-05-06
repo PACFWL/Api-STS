669873f7d8537cadfae67114dbfdf174891f5425
Pablo Flores <120192554+PACFWL@users.noreply.github.com>
2023-05-05 21:11:49 -0300

    Desaparecido ↑

--- sig1/src/main/java/com/fatec/sig1/model/Desaparecido.java
+++ sig1/src/main/java/com/fatec/sig1/model/Desaparecido.java
@@ -1,253 +1,504 @@
+
 package com.fatec.sig1.model;
 
 import java.text.DateFormat;
+
 import java.text.ParseException;
+
 import java.text.SimpleDateFormat;
 
 import javax.validation.constraints.NotBlank;
+
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.br.CPF;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 
 import org.joda.time.format.DateTimeFormatter;
 
-//import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.DateTime;
+
+import org.joda.time.format.DateTimeFormat;
+
+import jakarta.persistence.Column;
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.GeneratedValue;
+
+import jakarta.persistence.GenerationType;
+
+import jakarta.persistence.Id;
+
+// ...ds → ... desaparecido
+
+// ...dst → ... desaparecimento
+
+// (Opinião pessoal e creio válida não usar o isto "_" ) por causa findBy...
 
 @Entity
+
 public class Desaparecido {
-	
-	@Id
+
+		@Id
+
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id_desaparecido;
+
+    private Long idds;
+
 	@NotBlank(message = "Nome do desaparecido é requerido")	
-    private String nomeCompletoDesaparecido;
+
+    private String nomeds;
+
 	@Pattern(regexp = "^(0?[1-9]|[12][0-9]|3[01])[\\/-](0?[1-9]|1[012])[\\/-]\\d{4}$", message = "A data do desaparecimento deve estar no formato dd/MM/YYYY")    	
-	private String dataDeDesaparecimento;
+
+	private String datadedst;
+
 	@Pattern(regexp = "^(0?[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$", message = "A hora do desaparecimento deve estar no formato HH:mm:ss")    
-	private String horaDeDesaparecimento;
+
+	private String horadedst;
+
 	@Pattern(regexp = "^(0?[1-9]|[12][0-9]|3[01])[\\/-](0?[1-9]|1[012])[\\/-]\\d{4}$", message = "A data de nascimento deve estar no formato dd/MM/YYYY")    
-    private String dataDeNascimento;
-    private String dataDeCadastro;
+
+    private String datadenascimento;
+
+    private String datadecadastrods;
+
     @CPF
+
 	@Column(unique = true) // nao funciona com @Valid tem que tratar na camada de persistencia
-	private String cpfDoDesaparecido;
+
+	private String cpfdods;
+
 	@NotBlank(message = "Recompensa é requerido")	
-    private String recompensa;
+
+    private String recompensadods;
+
     
+
     /** 
+
     Usar Blob - Possibilidade
+
     **/
-	@NotBlank(message = "URL da Foto Principal é requerido")	
-    private String urlFotoPrincipal;
+
+	//@NotBlank(message = "URL da Foto Principal é requerido")	
+
+    //private String urlfotoprincipal;
+
+ 
+
 	@NotBlank(message = "Alt Text da Foto Principal é requerido")	
-    private String altTxtFotoPrincipal;
+
+    private String alttxtfotoprincipal;
+
 	@NotBlank(message = "Descrição do desaparecimento é requerido")	
-    private String descricaoDesaparecimento;
+
+    private String descricaododst;
+
 	@NotBlank(message = "Doença é requerido")	
-    private String doenca;
+
+    private String doencadods;
+
     @NotBlank(message = "Sexo M/F")
-    private String sexo_desaparecido;
+
+    private String sexodods;
+
     @NotBlank(message = "Cor de Pele é requerido")
-    private String corDePele;
+
+    private String cordepeledods;
+
+    @NotBlank(message = "Peso é requerido")
+
+    private double pesodods;
+
+    @NotBlank(message = "Tatuagem é requerido")
+
+    private boolean tatuagemdods;
+
+    @NotBlank(message = "Cicatriz é requerido")
+
+    private boolean cicatrizdods;
+
+    @ElementCollection
+
+	@Lob
+
+	private List<byte[]> imagens;
+
     
+
+    
+
+
+
 	public Desaparecido(
-				String nomeCompletoDesaparecido,
-				String dataDeNascimento,
-				String dataDeDesaparecimento,
-				String horaDeDesaparecimento,
-				String cpfDoDesaparecido,
-				String recompensa,
-				String urlFotoPrincipal,
-				String altTxtFotoPrincipal,
-				String descricaoDesaparecimento,
-				String doenca,
-				String sexo_desaparecido,
-				String corDePele) {
-		this.nomeCompletoDesaparecido = nomeCompletoDesaparecido;
-		setDataDeNascimento(dataDeNascimento);
-		setDataDeDesaparecimento(dataDeDesaparecimento);
-		setHoraDeDesaparecimento(horaDeDesaparecimento);
-		setDataDeCadastro(new DateTime());
-		this.cpfDoDesaparecido = cpfDoDesaparecido;
-        this.recompensa = recompensa;
-		this.urlFotoPrincipal = urlFotoPrincipal;
-		this.altTxtFotoPrincipal = altTxtFotoPrincipal;
-		this.descricaoDesaparecimento = descricaoDesaparecimento;
-		this.doenca = doenca;
-		this.sexo_desaparecido = sexo_desaparecido;
-		this.corDePele = corDePele;
+
+				String nomeds,
+
+				String datadedst,
+
+				String horadedst,
+
+				String datadenascimento,
+
+				String cpfdods,
+
+				String recompensadods,
+
+				String alttxtfotoprincipal,
+
+				String descricaododst,
+
+				String doencadods,
+
+				String sexodods,
+
+				String cordepeledods,
+
+				double pesodods,
+
+				boolean tatuagemdods,
+
+                boolean cicatrizdods,
+
+                List<byte[]> imagens) {
+
+		this.nomeds = nomeds;
+
+		setDatadedst(datadedst);
+
+		setHoradedst(horadedst);
+
+		setDatadenascimento(datadenascimento);
+
+		setDatadecadastrods(new DateTime());
+
+		this.cpfdods = cpfdods;
+
+        this.recompensadods = recompensadods;
+
+		this.alttxtfotoprincipal = alttxtfotoprincipal;
+
+		this.descricaododst = descricaododst;
+
+   	 this.doencadods = doencadods;
+
+    	this.sexodods = sexodods;
+
+    	this.cordepeledods = cordepeledods;
+
+    	this.pesodods = pesodods;
+
+   	 this.tatuagemdods = tatuagemdods;
+
+   	 this.cicatrizdods = cicatrizdods;
+
+   	 this.imagens = imagens;
+
 	}
+
 	
+
 	public Desaparecido()
+
 	{
+
+		this.imagens = new ArrayList<>();
+
 }
 
-	public Long getId_desaparecido() {
-		return id_desaparecido;
-	}
+public Long getIdds() {
 
-	public void setId_desaparecido(Long id_desaparecido) {
-		this.id_desaparecido = id_desaparecido;
-	}
+    return idds;
 
-	public String getNomeCompletoDesaparecido() {
-		return nomeCompletoDesaparecido;
-	}
+}
 
-	public void setNomeCompletoDesaparecido(String nomeCompletoDesaparecido) {
-		this.nomeCompletoDesaparecido = nomeCompletoDesaparecido;
-	}
+public void setIdds(Long idds) {
 
-	public String getDataDeNascimento() {
-		return dataDeNascimento;
-	}
+    this.idds = idds;
 
-	public void setDataDeNascimento(String dataDeNascimento) {
-		if (validaData(dataDeNascimento) == true) {
-			this.dataDeNascimento = dataDeNascimento;
+}
+
+public String getNomeds() {
+
+    return nomeds;
+
+}
+
+public void setNomeds(String nomeds) {
+
+    this.nomeds = nomeds;
+
+}
+
+public String getDatadedst() {
+
+    return datadedst;
+
+}
+
+public void setDatadedst(String datadedst) {
+
+    if (validaData(datadedst) == true) {
+
+			this.datadedst = datadedst;
+
 		} else {
-			throw new IllegalArgumentException("Data invalida");
+
+			throw new IllegalArgumentException("Data de desaparecimento inválida");
+
 			}
-		}
-		
-	public String getCpfDoDesaparecido() {
-		return cpfDoDesaparecido;
-	}
 
-	public void setCpfDoDesaparecido(String cpfDoDesaparecido) {
-		this.cpfDoDesaparecido = cpfDoDesaparecido;
-	}
+}
 
-	public String getRecompensa() {
-		return recompensa;
-	}
+public String getHoradedst() {
 
-	public void setRecompensa(String recompensa) {
-		this.recompensa = recompensa;
-	}
+    return horadedst;
 
-	public String getUrlFotoPrincipal() {
-		return urlFotoPrincipal;
-	}
+}
 
-	public void setUrlFotoPrincipal(String urlFotoPrincipal) {
-		this.urlFotoPrincipal = urlFotoPrincipal;
-	}
+public void setHoradedst(String horadedst) {
 
-	public String getAltTxtFotoPrincipal() {
-		return altTxtFotoPrincipal;
-	}
+    if (validaHora(horadedst) == true) {
 
-	public void setAltTxtFotoPrincipal(String altTxtFotoPrincipal) {
-		this.altTxtFotoPrincipal = altTxtFotoPrincipal;
-	}
+			this.horadedst = horadedst;
 
-	public String getDescricaoDesaparecimento() {
-		return descricaoDesaparecimento;
-	}
+		} else {
 
-	public void setDescricaoDesaparecimento(String descricaoDesaparecimento) {
-		this.descricaoDesaparecimento = descricaoDesaparecimento;
-	}
+			throw new IllegalArgumentException("Hora de desaparecimento inválida");
 
-	public String getDoenca() {
-		return doenca;
-	}
+			}
 
-	public void setDoenca(String doenca) {
-		this.doenca = doenca;
-	}
+}
 
-	public String getSexo_desaparecido() {
-		return sexo_desaparecido;
-	}
+public String getDatadenascimento() {
 
-	public void setSexo_desaparecido(String sexo_desaparecido) {
-		this.sexo_desaparecido = sexo_desaparecido;
-	}
+    return datadenascimento;
 
-	public String getCorDePele() {
-		return corDePele;
-	}
+}
 
-	public void setCorDePele(String corDePele) {
-		this.corDePele = corDePele;
-	}
-	
-	public boolean validaData(String data) {
+public void setDatadenascimento(String datadenascimento) {
+
+    this.datadenascimento = datadenascimento;
+
+}
+
+public String getDatadecadastrods() {
+
+    return datadecadastrods;
+
+}
+
+public void setDatadecadastrods(String datadecadastrods) {
+
+    this.datadecadastrods = obtemDataAtual(dataAtual);
+
+}
+
+public String getCpfdods() {
+
+    return cpfdods;
+
+}
+
+public void setCpfdods(String cpfdods) {
+
+    this.cpfdods = cpfdods;
+
+}
+
+public String getRecompensadods() {
+
+    return recompensadods;
+
+}
+
+public void setRecompensadods(String recompensadods) {
+
+    this.recompensadods = recompensadods;
+
+}
+
+public String getAlttxtfotoprincipal() {
+
+    return alttxtfotoprincipal;
+
+}
+
+public void setAlttxtfotoprincipal(String alttxtfotoprincipal) {
+
+    this.alttxtfotoprincipal = alttxtfotoprincipal;
+
+}
+
+public String getDescricaododst() {
+
+    return descricaododst;
+
+}
+
+public void setDescricaododst(String descricaododst) {
+
+    this.descricaododst = descricaododst;
+
+}
+
+public String getDoencadods() {
+
+    return doencadods;
+
+}
+
+public void setDoencadods(String doencadods) {
+
+    this.doencadods = doencadods;
+
+}
+
+public String getSexodods() {
+
+    return sexodods;
+
+}
+
+public void setSexodods(String sexodods) {
+
+    this.sexodods = sexodods;
+
+}
+
+public String getCordepeledods() {
+
+    return cordepeledods;
+
+}
+
+public void setCordepeledods(String cordepeledods) {
+
+    this.cordepeledods = cordepeledods;
+
+}
+
+public double getPesodods() {
+
+    return pesodods;
+
+}
+
+public void setPesodods(double pesodods) {
+
+    this.pesodods = pesodods;
+
+}
+
+public boolean isTatuagemdods() {
+
+    return tatuagemdods;
+
+}
+
+public void setTatuagemdods(boolean tatuagemdods) {
+
+    this.tatuagemdods = tatuagemdods;
+
+}
+
+public boolean isCicatrizdods() {
+
+    return cicatrizdods;
+
+}
+
+public void setCicatrizdods(boolean cicatrizdods) {
+
+    this.cicatrizdods = cicatrizdods;
+
+}
+
+public List<byte[]> getImagens() {
+
+    return imagens;
+
+}
+
+public void setImagens(List<byte[]> imagens) {
+
+    this.imagens = imagens;
+
+}
+
+public boolean validaData(String data) {
+
 		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
 		df.setLenient(false); //
+
 		try {
+
 			df.parse(data); // data válida (exemplo 30 fev - 31 nov)
+
 			return true;
+
 		} catch (ParseException ex) {
+
 			return false;
+
 		}
+
 	}
 
 	public String obtemDataAtual(DateTime dataAtual) {
+
 		DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/YYYY");
+
 		return dataAtual.toString(fmt);
+
 	}
 
     public boolean validaHora(String hora) {
+
     // create a new SimpleDateFormat object with the specified time format
+
     DateFormat df = new SimpleDateFormat("HH:mm:ss");
+
     
+
     // set the lenient mode of the time format to false
+
     df.setLenient(false);
+
     
+
     try {
+
         // attempt to parse the input time string using the time format
+
         df.parse(hora);
+
         // if the parse is successful, the time is valid and return true
+
         return true;
+
     } catch (ParseException ex) {
+
         // if the parse fails, the time is invalid and return false
+
         return false;
+
     }
+
 }
 
-
-	public String getDataDeDesaparecimento() {
-		return dataDeDesaparecimento;
-	}
-
-	public void setDataDeDesaparecimento(String dataDeDesaparecimento) {
-		if (validaData(dataDeDesaparecimento) == true) {
-			this.dataDeDesaparecimento = dataDeDesaparecimento;
-		} else {
-			throw new IllegalArgumentException("Data invalida");
-			}
-		}
-	
-	public String getHoraDeDesaparecimento() {
-		return horaDeDesaparecimento;
-	}
-
-	public void setHoraDeDesaparecimento(String horaDeDesaparecimento) {
-		if (validaHora(horaDeDesaparecimento) == true) {
-			this.horaDeDesaparecimento = horaDeDesaparecimento;
-		} else {
-			throw new IllegalArgumentException("Hora invalida");
-			}
-		}
-
-	public String getDataDeCadastro() {
-		return dataDeCadastro;
-	}
-
-	public void setDataDeCadastro(DateTime dataAtual) {
-		this.dataDeCadastro = obtemDataAtual(dataAtual);
-	}
-
-	
 }
+
+	
+	
+
+	
+
+	
+			
+    
+
+
+
