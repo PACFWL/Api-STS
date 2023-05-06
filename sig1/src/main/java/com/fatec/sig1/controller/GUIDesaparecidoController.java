1ad5b8925988511403bae4cdb718448a111ca301
Pablo Flores <120192554+PACFWL@users.noreply.github.com>
2023-05-06 13:34:15 -0300

    GUIDesaparecidoController ↑

--- sig1/src/main/java/com/fatec/sig1/controller/GUIDesaparecidoController.java
+++ sig1/src/main/java/com/fatec/sig1/controller/GUIDesaparecidoController.java
@@ -1,82 +1,203 @@
 package com.fatec.sig1.controller;
 
 import javax.validation.Valid;
+
 import org.apache.logging.log4j.LogManager;
+
 import org.apache.logging.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
+
 import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.servlet.ModelAndView;
+
 import com.fatec.sig1.model.Desaparecido;
+
 import com.fatec.sig1.services.MantemDesaparecido;
+
 @Controller
+
 @RequestMapping(path = "/sig")
+
 public class GUIDesaparecidoController {
+
 	Logger logger = LogManager.getLogger(GUIDesaparecidoController.class);
+
 	@Autowired
-	MantemDesaparecido servico_desaparecido;
+
+	MantemDesaparecido servicods;
+
 	@GetMapping("/desaparecidos")
-	public ModelAndView retornaFormDeConsultaTodosDesaparecidos() {
+
+	public ModelAndView retornaFormDeConsultaTodosDs() {
+
 		logger.info(">>>>>> controller consulta todos chamado" );
-		ModelAndView modelAndView_desaparecido = new ModelAndView("consultarDesaparecido");
-		modelAndView_desaparecido.addObject("desaparecidos", servico_desaparecido.consultaTodosDesaparecido());
-		return modelAndView_desaparecido;
+
+		ModelAndView modelAndViewds = new ModelAndView("consultarDesaparecido");
+
+		modelAndViewds.addObject("desaparecidos", servicods.consultaTodosDs());
+
+		return modelAndViewds;
+
 	}
+
 	@GetMapping("/desaparecido")
-	public ModelAndView retornaFormDeCadastroDe(Desaparecido desaparecido) {
-		ModelAndView mv_desaparecido = new ModelAndView("cadastrarDesaparecido");
-		mv_desaparecido.addObject("desaparecido", desaparecido);
-		return mv_desaparecido;
+
+	public ModelAndView retornaFormDeCadastroDeDs(Desaparecido desaparecido) {
+
+		ModelAndView mvds = new ModelAndView("cadastrarDesaparecido");
+
+		mvds.addObject("desaparecido", desaparecido);
+
+		return mvds;
+
 	}
-	@GetMapping("/desaparecidos/{cpf}") // diz ao metodo que ira responder a uma requisicao do tipo get
-	public ModelAndView retornaFormParaEditarDesaparecido(@PathVariable("cpf") String cpfDoDesaparecido) {
-		ModelAndView modelAndView_desaparecido = new ModelAndView("atualizarDesaparecido");
-		modelAndView_desaparecido.addObject("desaparecido", servico_desaparecido.consultaPorCpf(cpfDoDesaparecido).get()); // retorna um objeto do tipo cliente
-		return modelAndView_desaparecido; // addObject adiciona objetos para view
+
+	@GetMapping("/desaparecidos/{cpfdods}") // diz ao metodo que ira responder a uma requisicao do tipo get
+
+	public ModelAndView retornaFormParaEditarDs(@PathVariable("cpfdods") String cpfdods) {
+
+		ModelAndView modelAndViewds = new ModelAndView("atualizarDesaparecido");
+
+		modelAndViewds.addObject("desaparecido", servicods.consultaPorCpfdods(cpfdods).get()); // retorna um objeto do tipo cliente
+
+		return modelAndViewds; // addObject adiciona objetos para view
+
 	}
-	@GetMapping("/desaparecidos/id/{id_desaparecido}")
-	public ModelAndView excluirNoFormDeConsultaCliente(@PathVariable("id") Long id) {
-		servico.delete(id);
-		logger.info(">>>>>> 1. servico de exclusao chamado para o id =>  " + id);
-		ModelAndView modelAndView_desaparecido = new ModelAndView("consultarCliente");
-		modelAndView_desaparecido.addObject("clientes", servico.consultaTodos());
-		return modelAndView;
+
+	@GetMapping("/desaparecidos/idds/{idds}")
+
+	public ModelAndView excluirNoFormDeConsultaDs(@PathVariable("idds") Long idds) {
+
+		servicods.delete(idds);
+
+		logger.info(">>>>>> 1. servico de exclusao chamado para o id =>  " + idds);
+
+		ModelAndView modelAndViewds = new ModelAndView("consultarDesaparecido");
+
+		modelAndViewds.addObject("desaparecidos", servicods.consultaTodosDs());
+
+		return modelAndViewds;
+
 	}
-	@PostMapping("/clientes")
-	public ModelAndView save(@Valid Cliente cliente, BindingResult result) {
-		ModelAndView modelAndView = new ModelAndView("consultarCliente");
+
+	@PostMapping("/desaparecidos")
+
+	public ModelAndView saveDs(@Valid Desaparecido desaparecido, BindingResult result) {
+
+		ModelAndView modelAndViewds = new ModelAndView("consultarDesaparecido");
+
 		if (result.hasErrors()) {
-			modelAndView.setViewName("cadastrarCliente");
+
+			modelAndViewds.setViewName("cadastrarDesaparecido");
+
 		} else {
-			if (servico.save(cliente).isPresent()) {
+
+			if (servicods.saveDs(desaparecido).isPresent()) {
+
 				logger.info(">>>>>> controller chamou adastrar e consulta todos");
-				modelAndView.addObject("clientes", servico.consultaTodos());
+
+				modelAndViewds.addObject("desaparecidos", servicods.consultaTodosDs());
+
 			} else {
+
 				logger.info(">>>>>> controller cadastrar com dados invalidos");
-				modelAndView.setViewName("cadastrarCliente");
-				modelAndView.addObject("message", "Dados invalidos");
+
+				modelAndViewds.setViewName("cadastrarDesaparecido");
+
+				modelAndViewds.addObject("message", "Dados invalidos");
+
 			}
+
 		}
-		return modelAndView;
+
+		return modelAndViewds;
+
 	}
-	@PostMapping("/clientes/id/{id}")
-	public ModelAndView atualizaCliente(@PathVariable("id") Long id, @Valid Cliente cliente, BindingResult result) {
-		ModelAndView modelAndView = new ModelAndView("consultarCliente");
-		logger.info(">>>>>> servico para atualizacao de dados chamado");
+
+	/****/
+
+	@PostMapping("/disappeared/new")
+
+	public String saveNewDisappeared(@ModelAttribute("disappeared") @Valid DisappearedDTO disappearedDTO,
+
+	                                 BindingResult result, Model model,
+
+	                                 @RequestParam("files") MultipartFile[] files) throws IOException {
+
 		if (result.hasErrors()) {
-			logger.info(">>>>>> servico para atualizacao de dados com erro");
-			cliente.setId(id);
-			return new ModelAndView("atualizarCliente");
-		} else {
-			servico.atualiza(id,cliente);
-			modelAndView.addObject("clientes", servico.consultaTodos());
+
+			return "newDisappeared";
+
 		}
 
-		return modelAndView;
-	}
-}
+		Desaparecido desaparecido = new Desaparecido();
 
+		desaparecido.setNomeds(disappearedDTO.getNomeds());
+
+		desaparecido.setDatadedst(disappearedDTO.getDatadedst());
+
+		List<byte[]> images = new ArrayList<>();
+
+		for (MultipartFile file : files) {
+
+			images.add(file.getBytes());
+
+		}
+
+		desaparecido.setImagens(images);
+
+		keepDisappeared.save(desaparecido);
+
+		return "redirect:/disappeared";
+
+	}
+
+	
+
+	/****/
+
+	
+
+	
+
+	@PostMapping("/desaparecidos/idds/{idds}")
+
+	public ModelAndView atualizaDs(@PathVariable("idds") Long idds, @Valid Desaparecido desaparecido, BindingResult result) {
+
+		ModelAndView modelAndViewds = new ModelAndView("consultarDesaparecido");
+
+		logger.info(">>>>>> servico para atualizacao de dados chamado");
+
+		if (result.hasErrors()) {
+
+			logger.info(">>>>>> servico para atualizacao de dados com erro");
+
+			desaparecido.setIdds(idds);
+
+			return new ModelAndView("atualizarDesaparecido");
+
+		} else {
+
+			servicods.atualizaDs(idds, desaparecido);
+
+			modelAndViewds.addObject("desaparecidos", servicods.consultaTodosDs());
+
+		}
+
+		return modelAndViewds;
+
+	}
+
+}
